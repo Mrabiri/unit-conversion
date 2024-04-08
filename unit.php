8149228fe8d0ede0c443234e38df212f38c5f328
Mrabiri <38664260+Mrabiri@users.noreply.github.com>
2024-04-08 15:19:05 +0330

    Update unit.php

--- unit.php
+++ unit.php
@@ -9,25 +9,55 @@
         <label>مقدار:</label>
         <input type="text" name="value" required>
         <select name="from_unit">
-            <option value="cm">سانتیمتر (cm)</option>
-            <option value="m">متر (m)</option>
-            <option value="inch">اینچ (inch)</option>
-            <option value="ft">فوت (ft)</option>
-            <option value="g">گرم (g)</option>
-            <option value="kg">کیلوگرم (kg)</option>
-            <option value="fahrenheit">فارنهایت (Fahrenheit)</option>
-            <option value="celsius">سلسیوس (Celsius)</option>
+            <!-- انتخاب واحد مبدا -->
+            <optgroup label="طول">
+                <option value="foot">فوت (foot)</option>
+                <option value="inch">اینچ (inch)</option>
+                <option value="cm">سانتیمتر (cm)</option>
+            </optgroup>
+            <optgroup label="سرعت">
+                <option value="kmh">کیلومتر در ساعت (km/h)</option>
+                <option value="mph">مایل در ساعت (mph)</option>
+                <option value="yard">یارد بر ثانیه (yard/s)</option>
+            </optgroup>
+            <optgroup label="دما">
+                <option value="celsius">سلسیوس (Celsius)</option>
+                <option value="fahrenheit">فارنهایت (Fahrenheit)</option>
+            </optgroup>
+            <optgroup label="حجم">
+                <option value="liter">لیتر (liter)</option>
+                <option value="gallon">گالن (gallon)</option>
+            </optgroup>
+            <optgroup label="وزن">
+                <option value="kg">کیلوگرم (kg)</option>
+                <option value="pound">پوند (pound)</option>
+            </optgroup>
         </select>
         <label>به:</label>
         <select name="to_unit">
-            <option value="cm">سانتیمتر (cm)</option>
-            <option value="m">متر (m)</option>
-            <option value="inch">اینچ (inch)</option>
-            <option value="ft">فوت (ft)</option>
-            <option value="g">گرم (g)</option>
-            <option value="kg">کیلوگرم (kg)</option>
-            <option value="fahrenheit">فارنهایت (Fahrenheit)</option>
-            <option value="celsius">سلسیوس (Celsius)</option>
+            <!-- انتخاب واحد مقصد -->
+            <optgroup label="طول">
+                <option value="foot">فوت (foot)</option>
+                <option value="inch">اینچ (inch)</option>
+                <option value="cm">سانتیمتر (cm)</option>
+            </optgroup>
+            <optgroup label="سرعت">
+                <option value="kmh">کیلومتر در ساعت (km/h)</option>
+                <option value="mph">مایل در ساعت (mph)</option>
+                <option value="yard">یارد بر ثانیه (yard/s)</option>
+            </optgroup>
+            <optgroup label="دما">
+                <option value="celsius">سلسیوس (Celsius)</option>
+                <option value="fahrenheit">فارنهایت (Fahrenheit)</option>
+            </optgroup>
+            <optgroup label="حجم">
+                <option value="liter">لیتر (liter)</option>
+                <option value="gallon">گالن (gallon)</option>
+            </optgroup>
+            <optgroup label="وزن">
+                <option value="kg">کیلوگرم (kg)</option>
+                <option value="pound">پوند (pound)</option>
+            </optgroup>
         </select>
         <input type="submit" name="convert" value="تبدیل">
     </form>
@@ -36,29 +66,41 @@
     // تابع تبدیل واحدها
     function convert_units($value, $from_unit, $to_unit) {
         switch($from_unit) {
-            case 'cm':
-                $base_value = $value / 100; // تبدیل به متر
-                break;
-            case 'm':
-                $base_value = $value; // متر
+            case 'foot':
+                $base_value = $value * 30.48; // تبدیل به سانتیمتر
                 break;
             case 'inch':
-                $base_value = $value * 0.0254; // تبدیل به متر
+                $base_value = $value * 2.54; // تبدیل به سانتیمتر
                 break;
-            case 'ft':
-                $base_value = $value * 0.3048; // تبدیل به متر
+            case 'cm':
+                $base_value = $value; // سانتیمتر
                 break;
-            case 'g':
-                $base_value = $value / 1000; // تبدیل به کیلوگرم
+            case 'kmh':
+                $base_value = $value * 0.277778; // تبدیل به متر بر ثانیه
                 break;
-            case 'kg':
-                $base_value = $value; // کیلوگرم
+            case 'mph':
+                $base_value = $value * 0.44704; // تبدیل به متر بر ثانیه
+                break;
+            case 'yard':
+                $base_value = $value * 0.9144; // تبدیل به متر
+                break;
+            case 'celsius':
+                $base_value = $value; // سلسیوس
                 break;
             case 'fahrenheit':
                 $base_value = ($value - 32) * (5/9); // تبدیل به سلسیوس
                 break;
-            case 'celsius':
-                $base_value = $value; // سلسیوس
+            case 'liter':
+                $base_value = $value; // لیتر
+                break;
+            case 'gallon':
+                $base_value = $value * 3.78541; // تبدیل به لیتر
+                break;
+            case 'kg':
+                $base_value = $value; // کیلوگرم
+                break;
+            case 'pound':
+                $base_value = $value * 0.453592; // تبدیل به کیلوگرم
                 break;
             default:
                 $base_value = false; // واحد نامعتبر
@@ -69,22 +111,30 @@
         }
 
         switch($to_unit) {
-            case 'cm':
-                return $base_value * 100; // تبدیل به سانتیمتر
-            case 'm':
-                return $base_value; // متر
+            case 'foot':
+                return $base_value / 30.48; // تبدیل به فوت
             case 'inch':
-                return $base_value / 0.0254; // تبدیل به اینچ
-            case 'ft':
-                return $base_value / 0.3048; // تبدیل به فوت
-            case 'g':
-                return $base_value * 1000; // تبدیل به گرم
-            case 'kg':
-                return $base_value; // کیلوگرم
-            case 'fahrenheit':
-                return ($base_value * (9/5)) + 32; // تبدیل به فارنهایت
+                return $base_value / 2.54; // تبدیل به اینچ
+            case 'cm':
+                return $base_value; // سانتیمتر
+            case 'kmh':
+                return $base_value / 0.277778; // تبدیل به کیلومتر در ساعت
+            case 'mph':
+                return $base_value / 0.44704; // تبدیل به مایل در ساعت
+            case 'yard':
+                return $base_value / 0.9144; // تبدیل به یارد
             case 'celsius':
                 return $base_value; // سلسیوس
+            case 'fahrenheit':
+                return ($base_value * (9/5)) + 32; // تبدیل به فارنهایت
+            case 'liter':
+                return $base_value; // لیتر
+            case 'gallon':
+                return $base_value / 3.78541; // تبدیل به گالن
+            case 'kg':
+                return $base_value; // کیلوگرم
+            case 'pound':
+                return $base_value / 0.453592; // تبدیل به پوند
             default:
                 return false; // واحد نامعتبر
         }
